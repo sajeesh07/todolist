5f21097523fb679b3141fc4d4958ebe2dfcac691
Sajeesh M <101825124+sajeesh07@users.noreply.github.com>
2023-04-11 08:40:15 +0530

    Delete leap.js

--- leap.js
+++
@@ -1,4 +0,0 @@
-let year = 1996;
-if (year % 4 === 0 && year % 100 != 0) console.log(year + " is a Leap Year");
-else if (year % 400 === 0) console.log(year + " is a Leap Year");
-else console.log(year + " is not a Leap Year");
